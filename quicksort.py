e646825bbab6b8da4db933fe010dcb36654637b2
ToMMaaY <thuynh629@gmail.com>
2019-03-28 11:03:44 -0700

    completed quicksort

---
+++ quicksort.py
@@ -0,0 +1,11 @@
+def quicksort(arr):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+        return quicksort(less) + [pivot] + quicksort(greater)
+
+
+print(quicksort([4, 3, 6, 9, 7, 1, 5]))
